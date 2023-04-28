a98231bcfba68b63102bd386fdb68922d9ddf10d
Dejan <imdejan00@gmail.com>
2023-04-28 13:57:11 +0200

    Added cors policy for all adresses

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 const app = express()
+const cors = require('cors')
 
 // ROUTERS
 const userRouter = require('./routers/userRouter')
 
 // Parsers
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded())
 
 // router adding
